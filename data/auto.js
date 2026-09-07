a3658b13264b1259f394efe1c0e9ae717c4a568f
alessandroviani90 <alessandro.viani90@gmail.com>
2026-09-07 16:37:43 +0200

    Add car data to auto.js

--- data/auto.js
+++ data/auto.js
@@ -1,1 +1,74 @@
+const auto = [
+  {
+    id: 1,
+    marca: "Fiat",
+    modello: "500X",
+    versione: "1.3 MJT Connect 95 CV",
+    anno: 2021,
+    km: 72000,
+    prezzo: 12490,
+    alimentazione: "diesel",
+    cambio: "manuale",
+    potenza_cv: 95,
+    localita: "Padova",
+    stato: "disponibile"
+  },
+  {
+    id: 2,
+    marca: "Fiat",
+    modello: "500C",
+    versione: "1.2 GQ 69 CV Collection",
+    anno: 2015,
+    km: 79000,
+    prezzo: 8500,
+    alimentazione: "benzina",
+    cambio: "manuale",
+    potenza_cv: 69,
+    localita: "Padova",
+    stato: "disponibile"
+  },
+  {
+    id: 3,
+    marca: "Fiat",
+    modello: "Tipo",
+    versione: "5p 1.6 MJT S-Design 120 CV",
+    anno: 2018,
+    km: 79000,
+    prezzo: 9400,
+    alimentazione: "diesel",
+    cambio: "manuale",
+    potenza_cv: 120,
+    localita: "Padova",
+    stato: "disponibile"
+  },
+  {
+    id: 4,
+    marca: "Ford",
+    modello: "Tourneo Courier",
+    versione: "1.0 EcoBoost Sport",
+    anno: 2019,
+    km: 129000,
+    prezzo: 8500,
+    alimentazione: "benzina",
+    cambio: "manuale",
+    potenza_cv: 101,
+    localita: "Padova",
+    stato: "disponibile"
+  },
+  {
+    id: 5,
+    marca: "Fiat",
+    modello: "500",
+    versione: "1.3 MJT 16V GQ 95 CV",
+    anno: 2015,
+    km: 127000,
+    prezzo: 6500,
+    alimentazione: "diesel",
+    cambio: "manuale",
+    potenza_cv: 95,
+    localita: "Padova",
+    stato: "disponibile"
+  }
+];
 
+export default auto;
