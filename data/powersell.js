81a86e62ca34167dbdf73b92934db5dbe521dd28
alessandroviani90 <alessandro.viani90@gmail.com>
2026-09-07 07:37:15 +0200

    Expand powerSell object with client info and behavior

--- data/powersell.js
+++ data/powersell.js
@@ -1,3 +1,28 @@
-const powerSell = {};
+const powerSell = {
+  obiettivo: "Accompagnare il cliente nella ricerca e nell'individuazione dell'auto desiderata.",
+
+  cliente: {
+    informazioni_da_raccogliere: [
+      "auto già individuata dal cliente",
+      "marca",
+      "modello",
+      "versione o allestimento",
+      "budget",
+      "anno minimo",
+      "chilometraggio massimo",
+      "alimentazione",
+      "cambio",
+      "carrozzeria",
+      "zona di ricerca",
+      "esigenze e preferenze personali"
+    ]
+  },
+
+  comportamento: {
+    fare_domande: "Una o due domande alla volta, solo quando servono.",
+    accompagnare: true,
+    evitare_invenzioni: true
+  }
+};
 
 export default powerSell;
